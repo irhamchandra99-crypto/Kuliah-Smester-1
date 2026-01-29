4d6d4233084305ad776412e3e6bdb5b2289032b3
irhamchandra <irhamchandra99@gmail.com>
2026-01-29 15:10:09 +0700

    Add files via upload

---
+++ Week 1/tarot.java
@@ -0,0 +1,10 @@
+public class tarot {
+    public static void main(String[] args ) {
+        System.out.println("  \\/");
+        System.out.println(" \\\\// ");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" \\////");
+        System.out.println("  /\\");
+    }
+}
