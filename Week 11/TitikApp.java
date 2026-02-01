b9b3e5abbb823f6604ff891cf5ed6881f85a98f1
irhamchandra <irhamchandra99@gmail.com>
2026-02-01 21:55:01 +0700

    Add files via upload

---
+++ Week 11/TitikApp.java
@@ -0,0 +1,13 @@
+public class TitikApp {
+  public void Titik(){
+    int x;
+    int y;
+  }
+  public static void main(String[] args) {
+    Titik p1 = new Titik();
+    p1.x = 25;
+    p1.y = 12;
+    System.out.println(p1.x + " " +p1.y);
+    
+  }
+}
